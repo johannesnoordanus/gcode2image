2528629afa106a10b37c0e0c6007c9e6f03c53fa
johannesnoordanus <52376919+johannesnoordanus@users.noreply.github.com>
2023-08-22 16:23:22 +0200

    Version 2.2
    Added option --resolution
    Fix of grid size (did not change according to resolution)

--- gcode2image.py
+++ gcode2image.py
@@ -2,7 +2,7 @@
 """
 gcode2image: convert gcode to image.
 """
-__version__ = "2.1.0"
+__version__ = "2.2.0"
 
 import sys
 import re
@@ -10,6 +10,9 @@ import argparse
 from PIL import Image
 from skimage.draw import line as drawline
 import numpy as np
+
+# set to 10 pixels per mm
+DEFAULT_RESOLUTION = .1
 
 def gcode2image(args) -> np.array:
     """
@@ -21,8 +24,10 @@ def gcode2image(args) -> np.array:
     gcode = args.gcode
     G0_gray = 210 if args.showG0 else 0
 
-    X_pattern = "X[+\-]?[0-9]+(\.[0-9]+)?"
-    Y_pattern = "Y[+\-]?[0-9]+(\.[0-9]+)?"
+    float_pattern = "[+\-]?[0-9]+(\.[0-9]+)?"
+
+    X_pattern = f"X{float_pattern}"
+    Y_pattern = f"Y{float_pattern}"
     S_pattern = "S[0-9]+"
 
     gcode_pattern = "^(G0|G1|X|Y|M4|M3|M5|M2|S)"
@@ -177,9 +182,9 @@ def gcode2image(args) -> np.array:
                     # program end: stop
                     break
 
-    def find_min_max_X_Y_S():
+    def get_gcode_info():
         nonlocal gcode
-        min_max = { 'min_X': None, 'min_Y': None, 'max_X': None, 'max_Y': None, 'max_S': None }
+        gc_info = { 'min_X': None, 'min_Y': None, 'max_X': None, 'max_Y': None, 'max_S': None, 'pixelsize': None }
 
         while True:
             line = gcode.readline()
@@ -191,84 +196,104 @@ def gcode2image(args) -> np.array:
                 X = re.search(X_pattern,line)
                 if X:
                     X = float(X.group(0)[1:])
-                    min_max['min_X'] = X if not min_max['min_X'] or X < min_max['min_X'] else min_max['min_X']
-                    min_max['max_X'] = X if not min_max['max_X'] or X > min_max['max_X'] else min_max['max_X']
+                    gc_info['min_X'] = X if not gc_info['min_X'] or X < gc_info['min_X'] else gc_info['min_X']
+                    gc_info['max_X'] = X if not gc_info['max_X'] or X > gc_info['max_X'] else gc_info['max_X']
                 Y = re.search(Y_pattern,line)
                 if Y:
                     Y = float(Y.group(0)[1:])
-                    min_max['min_Y'] = Y if not min_max['min_Y'] or Y < min_max['min_Y'] else min_max['min_Y']
-                    min_max['max_Y'] = Y if not min_max['max_Y'] or Y > min_max['max_Y'] else min_max['max_Y']
+                    gc_info['min_Y'] = Y if not gc_info['min_Y'] or Y < gc_info['min_Y'] else gc_info['min_Y']
+                    gc_info['max_Y'] = Y if not gc_info['max_Y'] or Y > gc_info['max_Y'] else gc_info['max_Y']
 
                 S = re.search("S[0-9]+",line)
                 if S:
                     S = float(S.group(0)[1:])
-                    min_max['max_S'] = S if not min_max['max_S'] or S > min_max['max_S'] else min_max['max_S']
+                    gc_info['max_S'] = S if not gc_info['max_S'] or S > gc_info['max_S'] else gc_info['max_S']
 
-        return min_max
+            if line.find("pixelsize") >= 0:
+                # find a line that starts with ';' followed by 'pixelsize' (possibly a ':'), one or more spaces and a float
+                pixelsize = re.search(f"^;.*pixelsize:? +{float_pattern}", line)
+                if pixelsize:
+                    pixelsize = re.search(f"pixelsize:? +{float_pattern}", pixelsize.group()).group()
+                    gc_info['pixelsize'] = float(re.search(f"{float_pattern}", pixelsize).group())
 
-    # first pass: find min max of coordinates and S value
-    min_max = find_min_max_X_Y_S()
+        return gc_info
 
-    # set resolution to 254 DPI (assuming 1 gcode unit is 1 mm)
-    pixelsize = .1
+    # first pass: find min max of coordinates and S value, find pixelsize
+    gc_info = get_gcode_info()
+
+    if args.resolution is not None:
+        if gc_info['pixelsize'] and gc_info['pixelsize'] != args.resolution:
+            print(f"Note: conflicting settings: option '--resolution' {args.resolution} differs from {gc_info['pixelsize']} found in '{args.gcode.name}'")
+            print(f"pixelsize set to {args.resolution}")
+        pixelsize = args.resolution
+    elif gc_info['pixelsize']:
+        print(f"option '--resolution' is not supplied, pixel size set to {gc_info['pixelsize']} (found within '{args.gcode.name}')")
+        pixelsize = gc_info['pixelsize']
+    else:
+        print(f"pixelsize set to default {DEFAULT_RESOLUTION} (pixelsize is not set by option '--resolution' or found in '{args.gcode.name}')")
+        pixelsize = DEFAULT_RESOLUTION
 
     # save image lower left
-    img_min_X = min_max['min_X']
-    img_min_Y = min_max['min_Y']
+    img_min_X = gc_info['min_X']
+    img_min_Y = gc_info['min_Y']
 
     if args.showOrigin:
         # set possibly new lower left upper right for image to include the origin
-        min_max['min_X'] = 0 if 0 < min_max['min_X'] else min_max['min_X']
-        min_max['max_X'] = 0 if 0 > min_max['max_X'] else min_max['max_X']
-        min_max['min_Y'] = 0 if 0 < min_max['min_Y'] else min_max['min_Y']
-        min_max['max_Y'] = 0 if 0 > min_max['max_Y'] else min_max['max_Y']
+        gc_info['min_X'] = 0 if 0 < gc_info['min_X'] else gc_info['min_X']
+        gc_info['max_X'] = 0 if 0 > gc_info['max_X'] else gc_info['max_X']
+        gc_info['min_Y'] = 0 if 0 < gc_info['min_Y'] else gc_info['min_Y']
+        gc_info['max_Y'] = 0 if 0 > gc_info['max_Y'] else gc_info['max_Y']
 
         # X/Y start is used by 'draw_line' as offset
-        X_start = round(min_max['min_X']/pixelsize)
-        Y_start = round(min_max['min_Y']/pixelsize)
+        X_start = round(gc_info['min_X']/pixelsize)
+        Y_start = round(gc_info['min_Y']/pixelsize)
 
         # set current (x,y) and correct for possibly new origin
-        x = round((min_max['min_X'] + abs(min_max['min_X'] - img_min_X))/pixelsize)
-        y = round((min_max['min_Y'] + abs(min_max['min_Y'] - img_min_Y))/pixelsize)
+        x = round((gc_info['min_X'] + abs(gc_info['min_X'] - img_min_X))/pixelsize)
+        y = round((gc_info['min_Y'] + abs(gc_info['min_Y'] - img_min_Y))/pixelsize)
     else:
         # X/Y start is used by 'draw_line' as offset
-        X_start = round(min_max['min_X']/pixelsize)
-        Y_start = round(min_max['min_Y']/pixelsize)
+        X_start = round(gc_info['min_X']/pixelsize)
+        Y_start = round(gc_info['min_Y']/pixelsize)
 
         # set current (x,y)
         x = X_start
         y = Y_start
 
     # set image dimensions
-    img_height = abs(round(min_max['max_Y']/pixelsize) - round(min_max['min_Y']/pixelsize))
-    img_width = abs(round(min_max['max_X']/pixelsize) - round(min_max['min_X']/pixelsize))
+    img_height = abs(round(gc_info['max_Y']/pixelsize) - round(gc_info['min_Y']/pixelsize))
+    img_width = abs(round(gc_info['max_X']/pixelsize) - round(gc_info['min_X']/pixelsize))
 
     # set max S value (calibrate)
-    S_max = float(min_max['max_S'])
+    S_max = float(gc_info['max_S'])
 
     # init image
     image = np.full([img_height + 1, img_width + 1], 255, dtype=np.uint8)
 
+    print(f"image pixel size: {image.shape[1] - 1}x{image.shape[0] - 1} (WidthxHeight)")
+
     # show grid if requested
     if args.grid:
-        # make grid
-        for i in range(100,image.shape[1] if image.shape[1] >= image.shape[0] else image.shape[0],100):
+        # make grid 10mm x 10mm
+        for i in range(round(10/pixelsize),image.shape[1] if image.shape[1] >= image.shape[0] else image.shape[0],round(10/pixelsize)):
             image[i:i+1,:] = 180
             image[:,i:i+1] = 180
 
     # show origin if requested
     if args.showOrigin:
+        line_width_factor = max(round(.00053 * image.shape[1]),1)
+
         # calculate origin
         dX = -img_min_X if min(0.0, img_min_X) != 0.0 else 0.0
         dY = -img_min_Y if min(0.0, img_min_Y) != 0.0 else 0.0
 
         # get the right pixel ranges to keep within array borders
-        ra8 = pixel_range(8, dY, dX, pixelsize, img_height, img_width)
+        ra8 = pixel_range(8 * line_width_factor, dY, dX, pixelsize, img_height, img_width)
         # show origin
         image[ra8[0]:ra8[1], ra8[2]:ra8[3]] = 100
 
         # get pixel range of X/Y-axis lines
-        ra2 = pixel_range(2, dY, dX, pixelsize, img_height, img_width)
+        ra2 = pixel_range(2 * line_width_factor, dY, dX, pixelsize, img_height, img_width)
 
         # draw X/Y axis lines
         image[ra2[0]:ra2[1],0:img_width] = 100
@@ -277,14 +302,14 @@ def gcode2image(args) -> np.array:
         # draw X-axis line markers
         for i in range(0,round(img_width/pixelsize), 10):
             #image[dYmin8:dYplus8,i] = 0
-            ra = pixel_range(2, 0, i, pixelsize, img_height, img_width)
+            ra = pixel_range(2 * line_width_factor, 0, i, pixelsize, img_height, img_width)
             iXmin = ra[2]
             iXplus = ra[3]
             image[ra8[0]:ra8[1],iXmin:iXplus] = 100
 
         # draw Y-axis line markers
         for i in range(0,round(img_height/pixelsize), 10):
-            ra = pixel_range(2, i, 0, pixelsize, img_height, img_width)
+            ra = pixel_range(2 * line_width_factor, i, 0, pixelsize, img_height, img_width)
             iYmin = ra[0]
             iYplus = ra[1]
             image[iYmin:iYplus,ra8[2]:ra8[3]] = 100
@@ -309,10 +334,15 @@ def main() -> int:
     """
     main
     """
+
+    pixelsize_default = None
+
     # Define command line argument interface
     parser = argparse.ArgumentParser(description='Convert a gcode file to image.')
     parser.add_argument('gcode', type=argparse.FileType('r'), default = sys.stdin, help='name of gcode file to convert')
     parser.add_argument('image', type=argparse.FileType('w'), help='image out')
+    parser.add_argument('--resolution', default=pixelsize_default, metavar=f"<default: {DEFAULT_RESOLUTION}>",
+        type=float, help="define image resolution by pixel size (in mm^2): each image pixel is drawn this size")
     parser.add_argument('--showimage', action='store_true', default=False, help='show b&w converted image' )
     parser.add_argument('--showG0', action='store_true', default=False, help='show G0 moves' )
     parser.add_argument('--showOrigin', action='store_true', default=False, help='show image origin (0,0)' )
